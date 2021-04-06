dc7dd7d8fb2db821871656cc494292cc73566503
Cupertino Miranda <cmiranda@synopsys.com>
2021-04-06 13:16:06 +0100

    Usermode support for both ARC v2 and v3.
    Signed-off-by: Cupertino Miranda <cmiranda@synopsys.com>
    Signed-off-by: Vladimir Isaev <isaev@synopsys.com>

--- linux-user/syscall_defs.h
+++ linux-user/syscall_defs.h
@@ -74,7 +74,7 @@
     || defined(TARGET_M68K) || defined(TARGET_CRIS)                     \
     || defined(TARGET_S390X) || defined(TARGET_OPENRISC)                \
     || defined(TARGET_RISCV)                                            \
-    || defined(TARGET_XTENSA) || defined(TARGET_LOONGARCH64)
+    || defined(TARGET_XTENSA) || defined(TARGET_LOONGARCH64) || defined(TARGET_ARCV2) || defined(TARGET_ARCV3)
 
 #define TARGET_IOC_SIZEBITS     14
 #define TARGET_IOC_DIRBITS      2
@@ -1976,7 +1976,7 @@ struct target_stat64  {
 };
 
 #elif defined(TARGET_OPENRISC) \
-    || defined(TARGET_RISCV) || defined(TARGET_HEXAGON)
+    || defined(TARGET_RISCV) || defined(TARGET_HEXAGON) || defined(TARGET_ARCV2) || defined(TARGET_ARCV3)
 
 /* These are the asm-generic versions of the stat and stat64 structures */
 
@@ -2153,8 +2153,9 @@ struct target_statfs64 {
 };
 #elif (defined(TARGET_PPC64) || defined(TARGET_X86_64) ||       \
        defined(TARGET_SPARC64) || defined(TARGET_AARCH64) ||    \
-       defined(TARGET_RISCV) || defined(TARGET_LOONGARCH64)) && \
+       defined(TARGET_RISCV) || defined(TARGET_LOONGARCH64))  || defined(TARGET_ARCV2) || defined(TARGET_ARCV3) && \
     !defined(TARGET_ABI32)
+    
 struct target_statfs {
     abi_long f_type;
     abi_long f_bsize;
