85ab81eba2e5a5de0ec00ed3e0154bf2b3c4e681
Cupertino Miranda <cmiranda@synopsys.com>
2021-12-29 13:26:35 +0000

    Lots of changes to code merging both decodes and semantic functions.

--- linux-user/syscall_defs.h
+++ linux-user/syscall_defs.h
@@ -74,7 +74,7 @@
     || defined(TARGET_M68K) || defined(TARGET_CRIS)                     \
     || defined(TARGET_S390X) || defined(TARGET_OPENRISC)                \
     || defined(TARGET_RISCV)                                            \
-    || defined(TARGET_XTENSA) || defined(TARGET_LOONGARCH64) || defined(TARGET_ARCV2) || defined(TARGET_ARCV3)
+    || defined(TARGET_XTENSA) || defined(TARGET_LOONGARCH64) || defined(TARGET_ARC32) || defined(TARGET_ARC64)
 
 #define TARGET_IOC_SIZEBITS     14
 #define TARGET_IOC_DIRBITS      2
@@ -1976,7 +1976,7 @@ struct target_stat64  {
 };
 
 #elif defined(TARGET_OPENRISC) \
-    || defined(TARGET_RISCV) || defined(TARGET_HEXAGON) || defined(TARGET_ARCV2) || defined(TARGET_ARCV3)
+    || defined(TARGET_RISCV) || defined(TARGET_HEXAGON) || defined(TARGET_ARC32) || defined(TARGET_ARC64)
 
 /* These are the asm-generic versions of the stat and stat64 structures */
 
@@ -2004,7 +2004,7 @@ struct target_stat {
     abi_uint __unused5;
 };
 
-#if !defined(TARGET_RISCV64) && !defined(TARGET_ARCV3)
+#if !defined(TARGET_RISCV64) && !defined(TARGET_ARC64)
 #define TARGET_HAS_STRUCT_STAT64
 struct target_stat64 {
     abi_ullong st_dev;
@@ -2153,7 +2153,7 @@ struct target_statfs64 {
 };
 #elif (defined(TARGET_PPC64) || defined(TARGET_X86_64) ||       \
        defined(TARGET_SPARC64) || defined(TARGET_AARCH64) ||    \
-       defined(TARGET_RISCV) || defined(TARGET_LOONGARCH64))  || defined(TARGET_ARCV2) || defined(TARGET_ARCV3) && \
+       defined(TARGET_RISCV) || defined(TARGET_LOONGARCH64))  || defined(TARGET_ARC32) || defined(TARGET_ARC64) && \
     !defined(TARGET_ABI32)
     
 struct target_statfs {
